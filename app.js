d70917128fc50cfd54e44b1de687871a162c655f
GmRabbany <129893569+GmRabbany@users.noreply.github.com>
2023-07-03 20:49:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+ 
+//  practic 1
+// alert('shabdhan');
+// confirm('apni ki khaben?');
+// prompt('amnar nam ki?');
+// console.log(confirm('apni ki jaben?'))
+
+
+// practice 2
+// console.log(prompt('apnar nam ki?'))
+// console.log(prompt('apnar nam ki?'))
+// console.log(prompt('apnar nam ki?'))
+// console.log(prompt('apnar nam ki?'))
+// console.log(prompt('apnar nam ki?'))
+
+
+// practice 3
+//     var name = prompt('name')
+//     var food = prompt('food')
+//     var time = prompt('kokhon')
+  
+
+// console.log('my name is '+ name +' and i love '+ food+' shudhumatro '+ time);
+
+
+// practice 4
+//     let name = 'rabbany';
+//     let age = '23';
+//     let skill = 'wordpress';
+
+//     or 
+
+
+//     let name, age, skill;
+//     name = 'rabbany'
+//     age = '23'
+//     skill = 'wordpress'
+
+//     console.log(`my name is ${name} and I'm ${age} years old, my skill is ${skill}`)
+
+
+// practice five
+
+ let name = prompt('your name');
+ let age = prompt('your age');
+ let skill = prompt('your skill');
+ let job = prompt('your job');
+
+
+ console.log(`my name is ${name} and I'm ${age} years old and my skill is ${skill} and my job is ${job}`);
+
